a32ae036cd0d6f310abf5f8c7d48b1e738885e7d
kl.front <kevin.labatte0@gmail.com>
2023-03-02 11:59:20 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export const Header:React.FC = () => {
+    return <div>Build with 😁 by Kevin</div>
+}
---
+++ src/pages/blog/article1.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from "next";
+
+const BlogIndex:NextPage = () =>{
+    return<h1>Article 1 by Kevin</h1>;
+}
+
+export default BlogIndex;
