9f03cf044eca101058cb0ad38142c2e80b35e8b9
kl.front <kevin.labatte0@gmail.com>
2023-03-07 11:24:35 +0100

    build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Header } from "./Header"
 
 export const Navbar:React.FC = () => {
     return <header className="shadow-md flex">
-                <a className="m-4 font-bold font" href=""><span>Home</span></a>
+                <a className="m-4 font-bold font" href="index.tsx">Home</a>
                {/* <nav className="inline-block space-x-4 justify-end bg-red-600 ml-80 mx-40 ">
                     <ul className="mx-20 justify-end bg-yellow-400">
                         <li>Blog</li>
